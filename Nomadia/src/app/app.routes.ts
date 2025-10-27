bb4f67f543d9b8410a9b5c3d980ca93d082de49b
Mattew0800 <matiasoyhamburu24@gmail.com>
2025-10-27 19:38:51 -0300

    feat(NOM-4): add user-profile page and test base template

--- Nomadia/src/app/app.routes.ts
+++ Nomadia/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { LandingPage } from './pages/landing-page/landing-page';
 import { LoginPage } from './pages/login-page/login-page';
 import { RegisterPage } from './pages/register-page/register-page';
 import { ErrorPage } from './pages/error-page/error-page';
+import { UserProfile } from './pages/user-profile/user-profile';
+import { Test } from './pages/test/test';
 
 export const routes: Routes = [
     {path: 'landing', component: LandingPage},
     {path: 'login', component: LoginPage},
     {path: 'register', component: RegisterPage},
     {path: 'error', component: ErrorPage},
+    {path: 'profile', component: UserProfile},
+    {path: 'test', component: Test},
     {path: '', redirectTo: 'landing', pathMatch: 'full'},
     {path: '**', redirectTo: 'error' },
     
