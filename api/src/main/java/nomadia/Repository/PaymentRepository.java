47988d82e4ba63c24167eb3b48d27c7eec0cce83
Joan Ignasi Páez Pastrello <132505856+joancio12@users.noreply.github.com>
2026-02-16 18:31:49 -0300

    Feature/backend/nom 8 expenses (#42)
    * feat(NOM-8): add expenses with participants and payments
    
    * feat(NOM-8): add expense validation for trip membership
    
    * feat(NOM-8): improve activities list filtering and clean controllers
    
    * feat(NOM-8): refactor expense and dto
    
    * feat(NOM-8): quick creatrip fix
    
    * feat(NOM-8): handle optional password fields in user update flow
    
    * feat(NOM-8): quick fix
    
    * feat(NOM-8): implement expenses, balances and debt calculation
    
    * feat(NOM-8): quick response addition
    
    * feat(NOM-8): validate participant removal rules and properly map expense-activity in response
    
    * feat(NOM-8): code cleanup and implement endpoint for trip total calculation
    
    * feat(NOM-8): quick fix on delete trip
    
    * feat(NOM-8):  modularize user login register update logic and validation
    
    * feat(NOM-8):  add automatic debt settlement endpoint
    
    * feat(NOM-8): add user debt progress summary in trip debts endpoint

--- api/src/main/java/nomadia/Repository/PaymentRepository.java
+++ api/src/main/java/nomadia/Repository/PaymentRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByTripId(Long tripId);
+    int countByTripIdAndFromUserId(Long tripId, Long toUserId);
 }
