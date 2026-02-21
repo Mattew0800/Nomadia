3bff510a957319216265cf1a2151c4e2561b77c1
Nachoerbin <erbinignacio@gmail.com>
2026-02-21 13:03:41 -0300

    feat(NOM-7): filterChain finish

--- api/src/main/java/nomadia/Controller/UserController.java
+++ api/src/main/java/nomadia/Controller/UserController.java
@@ -16,7 +16,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/nomadia/user")
-@CrossOrigin (origins={"http://localhost:4200","http://localhost:8080"})
 public class UserController {
 
     private final UserService userService;
