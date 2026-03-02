3a15626d606f3a66201603e66cc62646f04aab50
Matias Oyhamburu <149548441+Mattew0800@users.noreply.github.com>
2026-03-02 14:27:21 -0300

    Fix formatting in environment.ts

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://nomadia-ptil.onrender.com'
+  apiUrl: 'https://nomadia-ptil.onrender.com',
   cloudinary: {
     cloudName: 'djw1jvnl5',
     uploadPreset: 'nomadia_users'
