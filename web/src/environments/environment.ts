3609ae3330639880b06f86e27408221d1457a25a
Matias Oyhamburu <149548441+Mattew0800@users.noreply.github.com>
2026-03-07 17:04:12 -0300

    fix(NOM-7): fix application-properties host and localhost conflict.

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://nomadia-ptil.onrender.com',
+  production: false,
+  apiUrl: 'http://localhost:8080',
   cloudinary: {
     cloudName: 'djw1jvnl5',
     uploadPreset: 'nomadia_users'
